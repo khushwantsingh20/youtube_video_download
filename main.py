382e17bb492f9e534c00af597fbb981bdfe2cead
khushwant singh <140226563+khushwantsingh20@users.noreply.github.com>
2024-11-09 10:38:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import yt_dlp
 
 def download_video(url):
     ydl_opts = {
-        'outtmpl': 'downloads/%(title)s.%(ext)s',  # Set download folder
+        'outtmpl': 'downloads/%(title)s.%(ext)s', 
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
